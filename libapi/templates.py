18a13845f6698189e8bc2cf8ffecaceafda3d114
Mark Vasilkov <mvasilkov@gmail.com>
2023-04-27 21:51:10 +0300

    Find variables

--- libapi/templates.py
+++ libapi/templates.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
+from collections import defaultdict
+from dataclasses import dataclass
 import re
+from typing import Any
 from urllib.parse import quote
+
+__all__ = ['pct_encode', 'URITemplate']
 
 # TODO RFC 6570 Reserved Expansion only accepts % in pct-encoded triplets
 reserved_characters = 0x21232425262728292A2B2C2F3A3B3D3F405B5D.to_bytes(19, 'big')
@@ -18,12 +23,33 @@ def pct_encode(a: str, reserved_expansion=False) -> str:
     return quote(a, safe=reserved_characters if reserved_expansion else b'', encoding='utf-8', errors='replace')
 
 
+@dataclass
+class Variable:
+    operator: str
+    start: int
+    end: int
+
+
 class URITemplate(str):
     '''
     RFC 6570 URI Template (Levels 1 and 2)
     https://datatracker.ietf.org/doc/html/rfc6570
     '''
 
+    variables: defaultdict[str, list[Variable]]
+
     def __new__(cls, a: str) -> URITemplate:
-        this = super().__new__(cls, a)
-        return this
+        self = super().__new__(cls, a)
+
+        self.variables = defaultdict(list)
+
+        for exp in expression_pattern.finditer(self):
+            operator = exp.group(1)
+            variable = exp.group(2)
+            start, end = exp.span()
+            self.variables[variable].append(Variable(operator, start, end))
+
+        return self
+
+    def expand(self, values: dict[str, Any]) -> str:
+        pass
